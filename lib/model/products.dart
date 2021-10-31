8482f2932d4e23780fd62a63f618a981657e2b5d
Moshood Kausargit <kausarmoshood@gmail.com>
2021-11-01 00:27:22 +0100

    Completion of first page

--- lib/model/products.dart
+++ lib/model/products.dart
@@ -1,48 +1,52 @@
+import 'package:flutter/material.dart';
+
 class ProductModel {
   final String? img;
   final String? name;
-  final String? bcolor;
+  final Color? bcolor;
 
   final String? amount;
 
   ProductModel({this.amount, this.name, this.img, this.bcolor});
 }
 
+
+
 List<ProductModel> productItems = [
   ProductModel(
     amount: '345',
     name: 'Red AirPods Max',
     img: 'assets/pink.png',
-    bcolor: 'Colors.yellow[100]',
+     bcolor: Colors.pink[100],
   ),
-   ProductModel(
+  ProductModel(
     amount: '345',
     name: 'Red AirPods Max',
     img: 'assets/black.png',
-    bcolor: 'Colors.yellow[100]',
+   bcolor: Colors.pink[100],
   ),
-  
   ProductModel(
     amount: '300',
     name: 'Blue AirPods Max',
     img: 'assets/blue.png',
-    bcolor: 'Colors.pink[100]',
+    bcolor: Colors.pink[100],
   ),
   ProductModel(
     amount: '400',
     name: 'Gold AirPods Max',
     img: 'assets/gold.png',
-    bcolor: 'Colors.pink[100]',
+    bcolor: Colors.pink[100],
   ),
   ProductModel(
     amount: '356',
     name: 'Purple AirPods Max',
     img: 'assets/purple.png',
-    bcolor: 'Colors.pink[100]',
+    bcolor: Colors.pink[100],
   ),
   ProductModel(
-      amount: '253',
-      name: 'Purple AirPods Max',
-      img: 'assets/pink.png',
-      bcolor: 'Colors.pink[100]',)
+    amount: '253',
+    name: 'Purple AirPods Max',
+    img: 'assets/pink.png',
+    bcolor: Colors.pink[100],
+  )
 ];
--- lib/screens/order.dart
+++ lib/screens/order.dart
@@ -18,168 +18,166 @@ class _OrderState extends State<Order> {
           body: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.only(left: 18.0, right: 18),
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => const DashBoard(),
-                          ));
-                    },
-                    icon: const Icon(
-                      Icons.chevron_left_outlined,
-                      color: Colors.black,
-                      size: 30,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const DashBoard(),
+                        ));
+                  },
+                  icon: const Icon(
+                    Icons.chevron_left_outlined,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                ),
+                Container(
+                    height: 350,
+                    width: MediaQuery.of(context).size.width,
+                    // ignore: prefer_const_constructors
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      image: const DecorationImage(
+                          image: AssetImage(
+                            'assets/pink.png',
+                          ),
+                          fit: BoxFit.contain),
                     ),
-                  ),
-                  Container(
-                      height: 350,
-                      width: MediaQuery.of(context).size.width,
-                      // ignore: prefer_const_constructors
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        image: const DecorationImage(
-                            image: AssetImage(
-                              'assets/pink.png',
-                            ),
-                            fit: BoxFit.contain),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Container(
-                                height: 3,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                    color: Colors.grey[300],
-                                    borderRadius: BorderRadius.circular(10))),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Container(
-                                height: 3,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                    color: Colors.grey[300],
-                                    borderRadius: BorderRadius.circular(10))),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Container(
-                                height: 3,
-                                width: 20,
-                                decoration: BoxDecoration(
-                                    color: Colors.black,
-                                    borderRadius: BorderRadius.circular(10))),
-                          )
-                        ],
-                      )),
-                  const SizedBox(height: 20),
-                  const Text('Headphones',
-                      style: TextStyle(color: Colors.grey)),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'AirPods Max',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: const [
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: CircleAvatar(
-                                radius: 15, backgroundColor: Colors.pink),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: CircleAvatar(
-                                radius: 15, backgroundColor: Colors.grey),
-                          ),
-                          CircleAvatar(
-                              radius: 15, backgroundColor: Colors.black),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Container(
-                              height: 20,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                              height: 3,
                               width: 20,
-                              child: const Center(
-                                child: Text("-",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.w800)),
-                              ),
                               decoration: BoxDecoration(
-                                border: Border.all(color: Colors.black),
-                              ),
-                            ),
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(right: 8.0),
-                            child: Text(
-                              '1',
-                            ),
-                          ),
-                          Container(
+                                  color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                              height: 3,
+                              width: 20,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(10))),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Container(
+                              height: 3,
+                              width: 20,
+                              decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(10))),
+                        )
+                      ],
+                    )),
+                const SizedBox(height: 20),
+                const Text('Headphones',
+                    style: TextStyle(color: Colors.grey)),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'AirPods Max',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              radius: 15, backgroundColor: Colors.pink),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                              radius: 15, backgroundColor: Colors.grey),
+                        ),
+                        CircleAvatar(
+                            radius: 15, backgroundColor: Colors.black),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Container(
                             height: 20,
                             width: 20,
-                            child: Center(child: Text("+")),
+                            child: const Center(
+                              child: Text("-",
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.w800)),
+                            ),
                             decoration: BoxDecoration(
                               border: Border.all(color: Colors.black),
                             ),
                           ),
-                        ],
-                      ),
-                      Text("\$549",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 25)),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                      "High-Fidelity Audio The Apple-designed driver delivers high-fidelity playback ...",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 18,
-                      )),
-                  SizedBox(height: 40),
-                  Container(
-                    height: 70,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: Center(
-                        child: Text("Buy Now",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white))),
-                  )
-                ],
-              ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(right: 8.0),
+                          child: Text(
+                            '1',
+                          ),
+                        ),
+                        Container(
+                          height: 20,
+                          width: 20,
+                          child: const Center(child: Text("+")),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Text("\$549",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 25)),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                    "High-Fidelity Audio The Apple-designed driver delivers high-fidelity playback ...",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    )),
+                const SizedBox(height: 40),
+                Container(
+                  height: 70,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Center(
+                      child: Text("Buy Now",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white))),
+                )
+              ],
             ),
           ),
         ))
